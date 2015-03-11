75ce8562724dd1e767e7074d9603e5ad75329845
George El-Haddad <george.dma@gmail.com>
2015-03-11 19:23:33 +0200

    Issue #1 Initial commit

---
+++ Maize/src/cimmyt/maize/MaizeScanner.java
@@ -0,0 +1,12 @@
+package cimmyt.maize;
+
+import ij.IJ;
+import ij.plugin.PlugIn;
+
+public class MaizeScanner implements PlugIn {
+
+	@Override
+    public void run(String arg) {
+		IJ.showMessage("test");
+	}
+}
